69b6fd5893fbfc29e012b5a095fa1d8b7cc7b74f
UPT TIK Unsri <ka.tik@unsri.ac.id>
2023-11-13 05:55:08 +0700

    Update dependencies and add links to home page

--- resources/views/billing/index.blade.php
+++ resources/views/billing/index.blade.php
@@ -5,7 +5,13 @@
 </div>
 <div class="container mt-5">
     <div class="row justify-content-left">
-        <div class="col-3 text-center mt-3">
+        <div class="col-md-3 text-center mt-3">
+            <a href="{{route('home')}}" class="text-decoration-none">
+                <img src="{{asset('images/form-deposit.svg')}}" alt="" width="100">
+                <h2>FORM DEPOSIT</h2>
+            </a>
+        </div>
+        <div class="col-md-3 text-center mt-3">
             <a href="{{route('home')}}" class="text-decoration-none">
                 <img src="{{asset('images/dashboard.svg')}}" alt="" width="100">
                 <h2>DASHBOARD</h2>
