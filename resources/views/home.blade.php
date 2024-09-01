3480bdce19e24cab15ec6d941de3640829fb5085
UPT TIK Unsri <ka.tik@unsri.ac.id>
2024-09-01 20:51:59 +0700

    Update home.blade.php with improved dashboard links layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -32,7 +32,7 @@
         <div class="col-md-3 text-center mb-5 mt-3">
             <a href="#" class="text-decoration-none">
                 <img src="{{asset('images/kosong.svg')}}" alt="" width="70">
-                <h4 class="mt-2">PURCHASE ORDER</h4>
+                <h4 class="mt-2">PURCHASE<br>ORDER</h4>
             </a>
         </div>
         <div class="col-md-3 text-center mb-5 mt-3">
