85782eb2b26c6e98f58b701c3f8ac2f8fd61374b
UPT TIK Unsri <ka.tik@unsri.ac.id>
2024-09-12 21:28:34 +0700

    Refactor home.blade.php to update 'LEGALITAS' section

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -82,9 +82,9 @@
             </div>
         </div>
         <div class="col-md-3 text-center mb-5 mt-3">
-            <a href="#" class="text-decoration-none">
-                <img src="{{asset('images/kosong.svg')}}" alt="" width="70">
-                <h4 class="mt-2">LEGALITAS</h4>
+            <a href="{{route('legalitas')}}" class="text-decoration-none">
+                <img src="{{asset('images/legalitas.svg')}}" alt="" width="70">
+                <h4 class="mt-3">LEGALITAS</h4>
             </a>
         </div>
         <div class="col-md-3 text-center mb-5 mt-3">
